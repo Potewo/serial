d564c0eb0705df7c387b724b017770811bdb9c14
Potewo <potewo@protonmail.com>
2021-03-27 22:34:37 +0900

    receive text from arduino through usb serial

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+  "log"
+  "github.com/tarm/serial"
+)
+
+func main() {
+  c := &serial.Config{
+    Name: "/dev/ttyACM0",
+    Baud: 9600,
+  }
+  s, err := serial.OpenPort(c)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  n, err := s.Write([]byte("R"))
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  data := make([]byte, 0)
+  for {
+    buf := make([]byte, 128)
+    n, err = s.Read(buf)
+    if err != nil {
+      log.Fatal(err)
+    }
+    if contains(buf[:n], '\x00') {
+      continue
+    }
+    data = append(data, buf[:n]...)
+    if contains(buf, '\n') {
+      break
+    }
+  }
+  log.Printf("%q", data)
+
+  _, err = s.Write([]byte("O"))
+  if err != nil {
+    log.Fatal(err)
+  }
+}
+
+func contains(a []byte, d byte) bool {
+  for _, v := range a {
+    if d == v {
+      return true
+    }
+  }
+  return false
+}
