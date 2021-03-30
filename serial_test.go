31b05bb12409ab5e31b78f5c4973c1b8cf973f69
Potewo <potewo@protonmail.com>
2021-03-30 19:59:42 +0900

    fix test code

--- serial_test.go
+++ serial_test.go
@@ -2,17 +2,37 @@ package serial
 
 import (
   "testing"
+  "time"
 )
 
-func SendTest(t *testing.T) {
+func TestReceiveSuccess(t *testing.T) {
   c := &Config {
     Name: "/dev/ttyACM0",
     Baud: 9600,
+    ReadTimeout: time.Second * 1,
   }
 
   s, err := OpenPort(c)
   if err != nil {
     t.Fatal(err)
   }
-  Send(s)
+  if err := Receive(s); err != nil {
+    t.Fatal(err)
+  }
+}
+
+func TestSendSuccess(t *testing.T) {
+  c := &Config {
+    Name: "/dev/ttyACM0",
+    Baud: 9600,
+    ReadTimeout: time.Second * 1,
+  }
+
+  s, err := OpenPort(c)
+  if err != nil {
+    t.Fatal(err)
+  }
+  if err := Send(s); err != nil {
+    t.Fatal(err)
+  }
 }
