4516192166c5349510bfa2edb4a13a3510f5b34a
Potewo <potewo@protonmail.com>
2021-04-05 14:56:54 +0900

    fix format

--- serial.go
+++ serial.go
@@ -1,8 +1,8 @@
 package serial
 
 import (
-  "time"
-  origin_serial "github.com/tarm/serial"
+	origin_serial "github.com/tarm/serial"
+	"time"
 )
 
 /*
@@ -25,106 +25,106 @@ func main() {
 */
 
 type Config struct {
-  Name string
-  Baud int
-  ReadTimeout time.Duration
+	Name        string
+	Baud        int
+	ReadTimeout time.Duration
 }
 
 func OpenPort(c *Config) (*origin_serial.Port, error) {
-  oc := &origin_serial.Config {Name: c.Name, Baud: c.Baud, ReadTimeout: c.ReadTimeout}
-  return origin_serial.OpenPort(oc)
+	oc := &origin_serial.Config{Name: c.Name, Baud: c.Baud, ReadTimeout: c.ReadTimeout}
+	return origin_serial.OpenPort(oc)
 }
 
 func contains(a []byte, d byte) bool {
-  for _, v := range a {
-    if d == v {
-      return true
-    }
-  }
-  return false
+	for _, v := range a {
+		if d == v {
+			return true
+		}
+	}
+	return false
 }
 
 func send(s *origin_serial.Port, d byte) error {
-  _, err := s.Write([]byte("S"))
-  if err != nil {
-    return err
-  }
+	_, err := s.Write([]byte("S"))
+	if err != nil {
+		return err
+	}
 
-  err = wait(s, 'R')
-  if err != nil {
-    return err
-  }
+	err = wait(s, 'R')
+	if err != nil {
+		return err
+	}
 
-  sendData := []byte{d}
-  _, err = s.Write(sendData)
-  if err != nil {
-    return err
-  }
+	sendData := []byte{d}
+	_, err = s.Write(sendData)
+	if err != nil {
+		return err
+	}
 
-  err = wait(s, 'O')
-  if err != nil {
-    return err
-  }
-  return nil
+	err = wait(s, 'O')
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func wait(s *origin_serial.Port, b byte) error {
-  for {
-    buf := make([]byte, 128)
-    n, err := s.Read(buf)
-    if err != nil {
-      return err
-    }
-    if contains(buf[:n], b) {
-      break
-    }
-  }
-  return nil
+	for {
+		buf := make([]byte, 128)
+		n, err := s.Read(buf)
+		if err != nil {
+			return err
+		}
+		if contains(buf[:n], b) {
+			break
+		}
+	}
+	return nil
 }
 
 func Send(s *origin_serial.Port, d []byte) error {
-  s.Write([]byte("N"))
-  err := wait(s, 'O')
-  if err != nil {
-    return err
-  }
+	s.Write([]byte("N"))
+	err := wait(s, 'O')
+	if err != nil {
+		return err
+	}
 
-  s.Write([]byte {uint8(len(d))})
+	s.Write([]byte{uint8(len(d))})
 
-  for _, di := range d {
-    err := send(s, di)
-    if err != nil {
-      return err
-    }
-  }
-  return nil
+	for _, di := range d {
+		err := send(s, di)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func Receive(s *origin_serial.Port) ([]byte, error) {
-  n, err := s.Write([]byte("R"))
-  if err != nil {
-    return nil, err
-  }
+	n, err := s.Write([]byte("R"))
+	if err != nil {
+		return nil, err
+	}
 
-  data := make([]byte, 0)
-  for {
-    buf := make([]byte, 128)
-    n, err = s.Read(buf)
-    if err != nil {
-      return nil, err
-    }
-    if contains(buf[:n], '\x00') {
-      continue
-    }
-    data = append(data, buf[:n]...)
-    if contains(buf, '\n') {
-      break
-    }
-  }
+	data := make([]byte, 0)
+	for {
+		buf := make([]byte, 128)
+		n, err = s.Read(buf)
+		if err != nil {
+			return nil, err
+		}
+		if contains(buf[:n], '\x00') {
+			continue
+		}
+		data = append(data, buf[:n]...)
+		if contains(buf, '\n') {
+			break
+		}
+	}
 
-  _, err = s.Write([]byte("O"))
-  if err != nil {
-    return nil, err
-  }
-  return data, nil
+	_, err = s.Write([]byte("O"))
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
